e16a336a914130826b3d68e9d6177cba24867f5b
Jonathan Nilsson <jonathan@voysys.se>
2024-03-23 19:53:04 +0100

    This version should work

--- push_image.sh
+++ push_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build -t lordjonil/loopia_update_ip:latest .
+docker build -t loopia_update_ip .
+docker image tag loopia_update_ip lordjonil/loopia_update_ip:latest
 docker image push lordjonil/loopia_update_ip:latest
